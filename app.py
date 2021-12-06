9ddb103b63c2b3401d3cfc5296ec2e0d5b8506cd
Wahyu Priyo Wicaksono <49259298+wpwwahyu@users.noreply.github.com>
2021-12-06 21:44:38 +0700

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import cv2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
-model = load_model('BatchNormModel.h5')
+model = load_model('Modelv2.h5')
 
 class_dict = {0:'PNEUMONIA', 1:'NORMAL'}
 
